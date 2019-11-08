de1c95ab3a8751c3d7b910c84cb7171fb5897afa
jason mortensen <zaross1@yahoo.com>
2019-11-08 01:44:02 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,34 @@
 # Your Code Here
+
+def map(arr)
+  
+  new_arr = []
+  
+  arr.each do |value|
+    new_arr << yield(value)
+  end 
+  
+  return new_arr 
+end
+
+def reduce(arr, start = 0)
+  
+  total = start 
+   
+  
+  arr.each do |value|
+     
+      result = yield(value, total)
+      if value.is_a?(Numeric)
+        total = result 
+      else 
+        total = total && result 
+      end 
+     
+      
+  end 
+  
+  return total 
+  
+  
+end 
