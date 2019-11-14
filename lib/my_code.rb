0b3457c7e1ae16a1f6d864f45878e115fb928d74
Brooke Bachman <bbachman1994@gmail.com>
2019-11-14 19:15:50 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+require 'pry'# Your Code Here
+def map(array)
+  empty = []
+  array.each do |item|
+    empty << yield(item)
+  end
+  return empty
+end
+
+def reduce(array, starting_point=nil)
+
+  if starting_point
+    total = starting_point
+    index = 0
+  else
+    total = array[0]
+    index = 1
+  end
+  while index < array.length
+    total = yield(total,array[index])
+    index += 1
+  end
+return total
+end
